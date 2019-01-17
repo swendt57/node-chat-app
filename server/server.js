7224d9b97d6c46d3932685d6577195986278bdaf
swendt57 <Lthr4ever>
2019-01-16 20:52:14 -0800

    added io connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
-let {authenticate} = require('./middleware/authenticate');
+// let {authenticate} = require('./middleware/authenticate');
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log("new user connected");
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected");
+    })
+});
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`app started on port ${port}`);
 });
 
