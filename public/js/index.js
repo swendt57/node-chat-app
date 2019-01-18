9819d27e7f3480241fc194593d4cd7684588a9bd
swendt57 <Lthr4ever>
2019-01-17 21:52:13 -0800

    ADDED FORM FOR MESSAGES

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('new message', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage', {
+//     from: 'John',
+//     text: 'Hello'
+// }, function(data) {
+//     console.log('Got it!!', data);
+// });
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    console.log('in the form processor');
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+})
