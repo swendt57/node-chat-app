af1bfb0442c7de8f448e39fb61cdbc92fc8b4a4e
swendt57 <Lthr4ever>
2019-01-17 16:01:32 -0800

    added Great New User and alter others

--- server/server.js
+++ server/server.js
@@ -16,9 +16,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("new user connected");
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user has joined!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+        // io.emit('newMessage', { //sent to everyone
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
