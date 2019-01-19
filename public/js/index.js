ef3c81ffa9d0d00d0b2047a34a9487ef71bb2341
swendt57 <Lthr4ever>
2019-01-18 16:13:09 -0800

    added css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -37,12 +37,14 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
-    console.log('in the form processor');
+
+    let messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-        alert('error!!');
+        messageTextbox.val('');
     });
 });
 
@@ -52,12 +54,16 @@ locationButton.on('click', function() {
         alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        })
+        });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 
