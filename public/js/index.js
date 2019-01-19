f3a3698fa71c92c22a0a93f1a94a9ed9a36d4ece
swendt57 <Lthr4ever>
2019-01-18 20:07:48 -0800

    added in styling and mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,24 +10,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('new message', message);
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>');
     let formattedTime = moment(message.createdAt).format('h:mm a');
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 // socket.emit('createMessage', {
--- public/js/libs/moment.js
+++ public/js/libs/moment.js
@@ -903,7 +903,7 @@
             this._shortMonthsParse = [];
         }
 
-        // TODO: add sorting
+
         // Sorting makes sure if one month (or abbr) is a prefix of another
         // see sorting in computeMonthsParse
         for (i = 0; i < 12; i++) {
@@ -943,7 +943,7 @@
                 value = toInt(value);
             } else {
                 value = mom.localeData().monthsParse(value);
-                // TODO: Another silent failure?
+
                 if (typeof value !== 'number') {
                     return mom;
                 }
@@ -1789,7 +1789,7 @@
 
     function loadLocale(name) {
         var oldLocale = null;
-        // TODO: Find a better way to register and load all the locales in Node
+
         if (!locales[name] && (typeof module !== 'undefined') &&
                 module && module.exports) {
             try {
@@ -2153,10 +2153,7 @@
             dow = 1;
             doy = 4;
 
-            // TODO: We need to take the current isoWeekYear, but that depends on
-            // how we interpret now (local, utc, fixed offset). So create
-            // a now version of current config (take local/utc/offset flags, and
-            // create now).
+
             weekYear = defaults(w.GG, config._a[YEAR], weekOfYear(local__createLocal(), 1, 4).year);
             week = defaults(w.W, 1);
             weekday = defaults(w.E, 1);
@@ -2203,7 +2200,7 @@
 
     // date from string and format string
     function configFromStringAndFormat(config) {
-        // TODO: Move this to another part of the creation flow to prevent circular deps
+
         if (config._f === utils_hooks__hooks.ISO_8601) {
             configFromISO(config);
             return;
@@ -2495,7 +2492,6 @@
         return res;
     }
 
-    // TODO: Use [].sort instead?
     function min () {
         var args = [].slice.call(arguments, 0);
 
@@ -2881,7 +2877,7 @@
         return res;
     }
 
-    // TODO: remove 'name' arg after deprecation is removed
+
     function createAdder(direction, name) {
         return function (val, period) {
             var dur, tmp;
@@ -3963,7 +3959,7 @@
         }
     }
 
-    // TODO: Use this.as('ms')?
+
     function duration_as__valueOf () {
         return (
             this._milliseconds +
