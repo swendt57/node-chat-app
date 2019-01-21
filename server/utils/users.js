3e4f348fcef73f65d78bde2c36e55a4394585d72
swendt57 <Lthr4ever>
2019-01-21 12:36:45 -0800

    added ability to add and remove users from rooms

--- server/utils/users.js
+++ server/utils/users.js
@@ -11,9 +11,32 @@ class Users {
         return user;
     };
 
-    removeUser() {
+    removeUser(id) {
+        let user = this.getUser(id);
 
+        if(user) {
+            this.users = this.users.filter((user) => user.id != id);
+        }
+
+        return user;
     }
+
+    getUser(id) {
+        return this.users.filter((user) => user.id  === id)[0];
+    }
+
+    getUserList(room) {
+        let users = this.users.filter((user) => {
+            return user.room === room;
+        });
+        let namesArray = users.map((user) => {
+            return user.name;
+        });
+
+        return namesArray;
+    }
+
+
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,25 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+    let users;
+
+    beforeEach(( ) => {
+        users = new Users();
+        users.users = [{
+            id: '1',
+            name: 'Steve',
+            room: 'Node Room'
+        }, {
+            id: '2',
+            name: 'Mike',
+            room: 'Another Room'
+        },
+        {
+            id: '3',
+            name: 'Chris',
+            room: 'Node Room'
+        }]
+    });
 
     it('should add new user', () => {
         let users = new Users();
@@ -15,5 +34,47 @@ describe('Users', () => {
 
         expect(users.users).toEqual([user]);
 
+    });
+
+    it('should remove a user', () => {
+        let userId = '1';
+        let user = users.removeUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(users.users.length).toBe(2);
+    });
+
+    it('should NOT remove user', () => {
+        let userId = 'something';
+        let user = users.removeUser(userId);
+
+        expect(user).toNotExist();
+        expect(users.users.length).toBe(3);
+    });
+
+    it('should find user', () => {
+        let userId = '2';
+        let user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+    });
+
+    it('should NOT find user', () => {
+        let userId = 'something';
+        let user = users.getUser(userId);
+
+        expect(user).toNotExist;;
+    });
+
+    it('should return names for Node Room', () => {
+        let userList = users.getUserList('Node Room');
+
+        expect(userList).toEqual(['Steve', 'Chris']);
+    });
+
+    it('should return names for Another Room', () => {
+        let userList = users.getUserList('Another Room');
+
+        expect(userList).toEqual(['Mike']);
     });
 });
