7403c7d6896f33fb8fad7387d707acb51a1a5ae9
swendt57 <Lthr4ever>
2019-01-21 17:50:20 -0800

    continued with adding and removing users

--- public/js/chat.js
+++ public/js/chat.js
@@ -33,6 +33,16 @@ socket.on('disconnect', function() {
     console.log('disconnected from server');
 });
 
+socket.on('updateUserList', function(users) {
+    let ol = jQuery('<ol></ol>');
+
+    users.forEach(function(user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
+
 socket.on('newMessage', function(message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
     let template = jQuery('#message-template').html();
--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
-
-// let {authenticate} = require('./middleware/authenticate');
+let users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -20,13 +21,16 @@ io.on('connection', (socket) => {
 
     socket.on('join', (params, callback) => {
         if( ! isRealString(params.name) || ! isRealString(params.room)) {
-            callback('Name and Room are required');
+            return callback('Name and Room are required');
         }
 
         socket.join(params.room);
-        //socket.leave('Room name');
+        // console.log(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
 
-
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        // console.log(users.getUserList(params.room));
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined!`));
         callback();
@@ -48,7 +52,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log("user disconnected");
+        let user = users.removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the building...`));
+        }
     });
 });
 
